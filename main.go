f6a88f6e0bbb9ffaeedb5b246711a7e880f3df22
ashfaq78602 <147393911+ashfaq78602@users.noreply.github.com>
2023-10-19 17:49:06 +0545

    Encryption

--- main.go
+++ main.go
@@ -1,31 +1,60 @@
 package main
 
 import (
-	"encoding/base64"
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"fmt"
 	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
 	msg := "Hello. I am happy to learning things really hands-on and thank you for sharing this info with me. I am really grateful and this will go a long way in helping me understand the basics of encryption. Lets gets started."
-	encoded := encoded(msg)
-	fmt.Println("Encoded message: ", encoded)
 
-	s, err := decode(encoded)
+	password := "ilovecats"
+	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		log.Println(err)
+		log.Panic("couldn't decrypt password")
 	}
-	fmt.Println("Decoded message: ", s)
+	bs := b[:16]
+	rslt, err := enDecode(bs, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(rslt)
+
+	rslt2, err := enDecode(bs, string(rslt))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(rslt2))
 }
 
-func encoded(msg string) string {
-	return base64.URLEncoding.EncodeToString([]byte(msg))
-}
-
-func decode(encoded string) (string, error) {
-	s, err := base64.URLEncoding.DecodeString(encoded)
+func enDecode(key []byte, input string) ([]byte, error) {
+	b, err := aes.NewCipher(key)
 	if err != nil {
-		return "", fmt.Errorf("couldnt decode string")
+		return nil, fmt.Errorf("couldn't newciper %w", err)
 	}
-	return string(s), nil
+
+	//initialization vector
+	iv := make([]byte, aes.BlockSize)
+
+	//create a cipher
+	s := cipher.NewCTR(b, iv)
+
+	buff := &bytes.Buffer{}
+	sw := cipher.StreamWriter{
+		S: s,
+		W: buff,
+	}
+	_, err = sw.Write([]byte(input))
+	if err != nil {
+		return nil, fmt.Errorf("couldn't sw.Write to streamwriter %w", err)
+	}
+
+	return buff.Bytes(), nil
 }
