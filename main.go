e99710b6991975717293f289ea045186c5400d02
ashfaq78602 <147393911+ashfaq78602@users.noreply.github.com>
2023-10-31 16:25:07 +0545

    Starting the oauth process

--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
 )
+
+var githubOauthConfig = &oauth2.Config{
+	ClientID:     "d2144988e98dad39a078",
+	ClientSecret: "17776eea6396b089501367b107029631b89a12f1",
+	Endpoint:     github.Endpoint,
+}
 
 func main() {
 	http.HandleFunc("/", index)
@@ -28,5 +37,6 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func startGithubOauth(w http.ResponseWriter, r *http.Request) {
-
+	redirectURL := githubOauthConfig.AuthCodeURL("0000")
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
 }
